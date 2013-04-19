eb93bcf1cc21e91183622b1851fd96030e16c10c
Chris Dueck <cdueck93@hotmail.com>
2013-04-19 08:15:03 -0400

    split Repa worldgen code into WorldGen. more work on camera controls

--- src/Automata.hs
+++ src/Automata.hs
@@ -1,44 +1,12 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 import Control.Monad
-import Data.Array.Repa
-import Data.Array.Repa.Repr.Cursored
+import Data.Array.Repa as A
 import Data.IORef
-import Graphics.Rendering.OpenGL hiding (index)
+import Graphics.Rendering.OpenGL
 import Graphics.UI.GLFW
-import Simplex
 import System.Exit
-
-type HeightMap = Array D DIM2 Float
-type Region a  = Array D DIM3 a
-type World  a  = Array C DIM3 (Region a)
-
-f2GLf :: Float -> GLfloat
-f2GLf = realToFrac
-
-i2GLf :: Int -> GLfloat
-i2GLf = fromIntegral
-
-i2f :: Int -> Float
-i2f = fromIntegral
-
-worldGen :: DIM3 -> World Float
-worldGen sh = makeCursored sh id addDim getRegion
-    where getRegion (Z :. i :. j :. k) =
-              let start = Z :. i - 32 :. j - 32 :. k - 32
-                  end   = Z :. i + 32 :. j + 32 :. k + 32
-              in extract start end world
-          world = regionGen sh
-
-regionGen :: DIM3 -> Region Float
-regionGen sh = noise 3 22 +^ noise 3 33 +^ noise 3 44
-    where noise octave freq = fromFunction sh $ \(Z :. x :. y :. z) ->
-            harmonic3D octave freq (i2f x) (i2f y) (i2f z)
-
-heightMap :: DIM2 -> HeightMap
-heightMap sh = noise 3 10 +^ noise 3 15 +^ noise 3 20 +^ noise 3 3 +^ noise 3 0.25
-    where noise octave freq = fromFunction sh $ \(Z :. x :. y) ->
-              harmonic2D octave freq (i2f x) (i2f y)
+import WorldGen
 
 class Renderable a where
     render :: a -> IO ()
@@ -46,9 +14,29 @@ class Renderable a where
 instance Renderable HeightMap where
     render = renderPrimitive Quads . renderHeightMap
 
+{-instance Num a => Num (Vector3 a) where-}
+    {-(Vector3 a b c) + (Vector3 x y z) = Vector3 (a + x) (b + y) (c + z)-}
+    {-(Vector3 a b c) * (Vector3 x y z) = -- cross product-}
+        {-Vector3 (b * z - c * y) (c * x - a * z) (a * y - b * x)-}
+    {-fromInteger x = Vector3 (fromIntegral x) (fromIntegral x) (fromIntegral x)-}
+
+data Camera = Camera
+    { cameraPos :: Vector3 GLdouble
+    , cameraRot :: Vector2 GLdouble
+    }
+
+f2GLf :: Float -> GLfloat
+f2GLf = realToFrac
+
+i2GLf :: Int -> GLfloat
+i2GLf = fromIntegral
+
+toRad :: Floating a => a -> a
+toRad deg = deg * pi / 180
+
 renderHeightMap :: HeightMap -> IO ()
 renderHeightMap arr = do
-    let delta = 2
+    let delta = 1
         Z :. x :. y = extent arr
     forM_ [0,delta..x] $ \i ->
         forM_ [0,delta..y] $ \j -> do
@@ -57,80 +45,92 @@ renderHeightMap arr = do
             renderVertex (i + delta) (j + delta)
             renderVertex (i + delta)  j
     where renderVertex i j = do
-              let h = max 0 $ index arr (ix2 i j)
-              color  $ Color3 0 0 h
-              vertex $ Vertex3 (10 * i2GLf i) (30 * h) (10 * i2GLf (-j))
+            let h = f2GLf . max 0 $ A.index arr (ix2 i j)
+            color  $ Color3 0 0 h
+            vertex $ Vertex3 (10 * i2GLf i) (30 * h) (10 * i2GLf (-j))
 
-initFog :: IO ()
-initFog = do
-    hint Fog $= DontCare
-    fogMode  $= Linear 0 1
-    fogColor $= Color4 0.2 0.2 0.2 0.0
-    fog      $= Enabled
+{-initFog :: IO ()-}
+{-initFog = do-}
+    {-hint Fog $= Nicest-}
+    {-fogMode  $= Linear 0 1-}
+    {-fogColor $= Color4 0.2 0.2 0.2 0.0-}
+    {-fog      $= Enabled-}
 
-initLighting :: IO ()
-initLighting = do
-    lighting           $= Enabled
-    light (Light 0)    $= Enabled
-    position (Light 0) $= Vertex4 0 5 10 1
-    lightModelAmbient  $= Color4 0.2 0.2 0.2 1
+{-initLighting :: IO ()-}
+{-initLighting = do-}
+    {-lighting           $= Enabled-}
+    {-light (Light 0)    $= Enabled-}
+    {-position (Light 0) $= Vertex4 0 5 10 1-}
+    {-lightModelAmbient  $= Color4 0.2 0.2 0.2 1-}
 
 initGL :: IO ()
 initGL = do
-    clearColor $= Color4 0.1 0.1 0.1 1.0
+    clearColor $= Color4 0.0 0.0 0.0 1.0
     cullFace   $= Just Front
     depthFunc  $= Just Less
     shadeModel $= Smooth
-    hint PerspectiveCorrection $= DontCare
+    hint PerspectiveCorrection $= Nicest
 
-initGLFW :: IO ()
-initGLFW = do
-    enableSpecial KeyRepeat
-
-renderScene :: Renderable a => IORef (Vertex3 GLdouble) -> a -> IO ()
+renderScene :: Renderable a => IORef Camera -> a -> IO ()
 renderScene camera scene = do
     clear [ColorBuffer, DepthBuffer]
     matrixMode $= Projection
     loadIdentity
-    cameraPos <- get camera
     perspective 45 1.5 1 1000
-    lookAt cameraPos (Vertex3 640 0 (-640)) (Vector3 0 1 0)
+    matrixMode $= Modelview 0
+    loadIdentity
+    Camera pos (Vector2 rotX rotY) <- get camera
+    {-rotate 20   $ Vector3 1 0 (0 :: GLdouble)-}
+    rotate rotY $ Vector3 0 1 (0 :: GLdouble)
+    translate pos
     render scene
     swapBuffers
 
-myKeyCallback :: IORef (Vertex3 GLdouble) -> KeyCallback
+myKeyCallback :: IORef Camera -> KeyCallback
 myKeyCallback camera key state = do
-    Vertex3 dx dy dz <- get camera
+    Camera (Vector3 dx dy dz) rot@(Vector2 rotX rotY) <- get camera
     when (state == Press) $ case key of
-        CharKey c    -> case c of
-            '-'   -> camera $= Vertex3 dx (dy - 10) dz
-            '='   -> camera $= Vertex3 dx (dy + 10) dz
-            _     -> return ()
-        SpecialKey k -> case k of
-            ESC   -> closeWindow >> terminate >> exitSuccess
-            UP    -> camera $= Vertex3 dx dy (dz - 10)
-            DOWN  -> camera $= Vertex3 dx dy (dz + 10)
-            LEFT  -> camera $= Vertex3 (dx - 10) dy dz
-            RIGHT -> camera $= Vertex3 (dx + 10) dy dz
-            _     -> return ()
+        CharKey c -> case c of
+            '-' -> camera $= Camera (Vector3 dx (dy + 10) dz) rot
+            '=' -> camera $= Camera (Vector3 dx (dy - 10) dz) rot
 
-myMousePosCallback :: IORef (Vertex3 GLdouble) -> MousePosCallback
-myMousePosCallback _ (Position x y) = return ()
+            'W' -> camera $= Camera (Vector3 (dx + 10 * sin (toRad rotY)) dy (dz + 10 * cos (toRad rotY))) rot
+            'S' -> camera $= Camera (Vector3 (dx - 10 * sin (toRad rotY)) dy (dz - 10 * cos (toRad rotY))) rot
+            'A' -> camera $= Camera (Vector3 (dx + 10) dy dz) rot
+            'D' -> camera $= Camera (Vector3 (dx - 10) dy dz) rot
+            _   -> return ()
+        SpecialKey k -> case k of
+            ESC -> closeWindow >> terminate >> exitSuccess
+            _   -> return ()
+
+myMouseCallback :: IORef Camera -> MousePosCallback
+myMouseCallback camera (Position x y) = do
+    Camera pos (Vector2 rotX rotY) <- get camera
+    Size w h <- get windowSize
+
+    let midX = w `quot` 2
+        midY = h `quot` 2
+        dx = fromIntegral $ x - midX
+        dy = fromIntegral $ y - midX
+
+    camera   $= Camera pos (Vector2 (rotX + dy) (rotY + dx))
+    mousePos $= Position midX midY
 
 main :: IO ()
 main = do
     initialize
     openWindow (Size 1080 720) [DisplayDepthBits 32, DisplayRGBBits 8 8 8] Window
     windowTitle $= "Automata"
+    enableSpecial KeyRepeat
+    disableSpecial MouseCursor
     initGL
-    initGLFW
 
-    let world = heightMap (ix2 128 128)
-    camera <- newIORef (Vertex3 64 64 64)
-    eye    <- newIORef (Vertex3 64 0 (-64))
+    camera <- newIORef $ Camera (Vector3 (-640) 10 (-340)) (Vector2 0 0)
+
+    let world  = heightMap (ix2 128 128)
 
     windowSizeCallback $= \s -> viewport $= (Position 0 0, s)
-    {-mousePosCallback   $= myMousePosCallback eye-}
+    mousePosCallback   $= myMouseCallback camera
     keyCallback        $= myKeyCallback camera
 
     forever $ renderScene camera world
---
+++ src/WorldGen.hs
@@ -0,0 +1,13 @@
+module WorldGen (HeightMap, heightMap) where
+import Data.Array.Repa
+import Simplex
+
+type HeightMap = Array D DIM2 Float
+
+i2f :: Int -> Float
+i2f = fromIntegral
+
+heightMap :: DIM2 -> HeightMap
+heightMap sh = noise 3 10 +^ noise 3 20 +^ noise 3 40 +^ noise 5 0.25
+    where noise octave freq = fromFunction sh $ \(Z :. x :. y) ->
+              harmonic2D octave freq (i2f x) (i2f y)
