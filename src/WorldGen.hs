86818bce7852be15194e907e94f8e5a564f0bbf6
Chris Dueck <cdueck93@hotmail.com>
2013-04-20 20:23:17 -0400

    switch the Manifest repr, to avoid recomputing heightmap when rendering

--- src/WorldGen.hs
+++ src/WorldGen.hs
@@ -2,12 +2,12 @@ module WorldGen (HeightMap, heightMap) where
 import Data.Array.Repa
 import Simplex
 
-type HeightMap = Array D DIM2 Float
+type HeightMap = Array U DIM2 Float
 
 i2f :: Int -> Float
 i2f = fromIntegral
 
 heightMap :: DIM2 -> HeightMap
-heightMap sh = noise 3 10 +^ noise 3 20 +^ noise 3 40 +^ noise 5 0.25
+heightMap sh = computeUnboxedS $ noise 3 10 +^ noise 3 20 +^ noise 3 40 +^ noise 5 0.25
     where noise octave freq = fromFunction sh $ \(Z :. x :. y) ->
               harmonic2D octave freq (i2f x) (i2f y)
