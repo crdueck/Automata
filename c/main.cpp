ab3cf4057d07e1b57072847bbf777910192b7e08
crdueck <chris@ubuntu.exactearth>
2013-04-24 16:42:45 -0400

    rendering works, working on camera controls and movement

--- c/main.cpp
+++ c/main.cpp
@@ -1,4 +1,4 @@
-#include <cstdlib>
+#include <cassert>
 #include <GL/glew.h>
 #include <GL/glfw.h>
 #include <glm/glm.hpp>
@@ -6,105 +6,35 @@
 #include <glm/gtc/noise.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#define INIT_CAMERA(camera) do \
-{ \
-    (camera).x = 0.0f; \
-    (camera).y = 0.0f; \
-    (camera).z = 0.0f; \
-    (camera).rotX = 0.0f; \
-    (camera).rotY = 0.0f; \
-} while(0)
+#include "callbacks.hpp"
 
-typedef struct camera_t
-{
-    float x;
-    float y;
-    float z;
-    float rotX;
-    float rotY;
-} Camera;
-
-Camera g_camera;
-
-const char *vertexShader =
-    "#version 330\n"
-    "in vec3 position;\n"
+const char *vertexSource =
+    "#version 120\n"
+    //"in vec3 position;\n" // GLSL 3.30
+    "attribute vec3 position;\n" // GLSL 1.20
     "uniform mat4 model;\n"
-    "uniform mat4 view;\n"
     "uniform mat4 proj;\n"
-    "smooth out vec3 color;\n"
+    //"out vec3 color;\n" // GLSL 3.30
+    "varying vec3 color;\n" // GLSL 1.20
     "void main() {\n"
-    //"   gl_Position = proj * view * model * vec4(position, 1.0);\n"
-    "   gl_Position = model * vec4(position, 1.0);\n"
-    //"   gl_Position = vec4(position, 1.0);\n"
+    "   gl_Position = proj * model * vec4(position, 1.0);\n"
     "   color = position;\n"
     "}";
 
-const char *fragmentShader =
-    "#version 330\n"
-    "smooth in vec3 color;\n"
+const char *fragmentSource =
+    "#version 120\n"
+    //"in vec3 color;\n" // GLSL 3.30
+    "varying vec3 color;\n" // GLSL 1.20
     "void main() {\n"
-    "   gl_FragColor = vec4(color, 1.0);\n"
+    "   gl_FragColor = vec4(0.0, 0.0, color.b, 1.0);\n"
     "}";
-
-void GLFWCALL keyCallback(int key, int action)
-{
-    if (action == GLFW_PRESS) {
-        switch (key) {
-            case 'W':
-                g_camera.x += sin(g_camera.rotY);
-                g_camera.z -= cos(g_camera.rotY);
-                break;
-            case 'S':
-                g_camera.x -= sin(g_camera.rotY);
-                g_camera.z += cos(g_camera.rotY);
-                break;
-            case 'A':
-                g_camera.x -= cos(g_camera.rotY);
-                g_camera.z -= sin(g_camera.rotY);
-                break;
-            case 'D':
-                g_camera.x += cos(g_camera.rotY);
-                g_camera.z += sin(g_camera.rotY);
-                break;
-            case '-':
-                g_camera.y += 1;
-                break;
-            case '=':
-                g_camera.y -= 1;
-                break;
-            default:
-                break;
-        }
-    }
-}
-
-void GLFWCALL mousePosCallback(int x, int y)
-{
-    int w, h;
-    glfwGetWindowSize(&w, &h);
-
-    int midX = w * 0.5f;
-    int midY = h * 0.5f;
-
-    g_camera.rotX += y - midY;
-    g_camera.rotY += midX - x;
-
-    glm::clamp(g_camera.rotX, -90.0f, 90.0f);
-    glm::clamp(g_camera.rotY, -180.0f, 180.0f);
-
-    glfwSetMousePos(midX, midY);
-}
-
-void GLFWCALL windowSizeCallback(int w, int h)
-{
-    glViewport(0, 0, w, h);
-}
 
 void glInit()
 {
     glClearColor(0.5f, 0.7f, 1.0f, 1.0f);
+    glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
+    glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
     glLineWidth(1.5f);
 
@@ -117,19 +47,10 @@ void glInit()
     glFogfv(GL_FOG_COLOR, fogColor);
 }
 
-#define INIT_HEIGHTMAP(map, width, height) do \
-{ \
-    for (size_t i = 0; i < (width); i++) { \
-        for (size_t j = 0; j < (height); j++) { \
-            (map)[i * 3 + j * 3 * (width) + 0] = (float)i; \
-            (map)[i * 3 + j * 3 * (width) + 1] = glm::simplex(glm::vec2(i, j)); \
-            (map)[i * 3 + j * 3 * (width) + 2] = (float)j; \
-        } \
-    } \
-} while(0)
+static const size_t WIDTH = 64;
+static const size_t HEIGHT = 64;
 
-static const size_t WIDTH = 10;
-static const size_t HEIGHT = 10;
+Camera g_camera;
 
 int main(int argc, char const *argv[])
 {
@@ -148,28 +69,37 @@ int main(int argc, char const *argv[])
     INIT_CAMERA(g_camera);
 
     float heightmap[WIDTH * HEIGHT * 3];
-    INIT_HEIGHTMAP(heightmap, WIDTH, HEIGHT);
 
-    //for (size_t i = 0; i < WIDTH * HEIGHT * 3; i += 3) {
-        //float x = heightmap[i + 0];
-        //float y = heightmap[i + 1];
-        //float z = heightmap[i + 2];
-        //printf("(%f, %f, %f)\n", x, y, z);
-    //}
+    for (size_t i = 0; i < WIDTH; ++i) {
+        for (size_t j = 0; j < HEIGHT; ++j) {
+
+            //int ix1 = i * 3 + j * 3 * WIDTH + 0;
+            //int ix2 = i * 3 + j * 3 * WIDTH + 1;
+            //int ix3 = i * 3 + j * 3 * WIDTH + 2;
+            //printf("(%d, %d, %d)\n", ix1, ix2, ix3);
+
+            heightmap[i * 3 + j * 3 * WIDTH + 0] = (float)i;
+            heightmap[i * 3 + j * 3 * WIDTH + 1] = glm::simplex(glm::vec2(i, j));
+            heightmap[i * 3 + j * 3 * WIDTH + 2] = (float)j;
+
+        }
+    }
 
     GLuint indices[WIDTH * HEIGHT * 4];
 
-    int ix = 0;
-    for (size_t i = 0; i < WIDTH; i++) {
-        for (size_t j = 0; j < HEIGHT; j++) {
+    size_t ix = 0;
+    for (size_t i = 0; i < WIDTH - 1; i++) {
+        for (size_t j = 0; j < HEIGHT - 1; j++) {
+
             indices[ix++] = (i + 0) + (j + 0) * WIDTH;
             indices[ix++] = (i + 0) + (j + 1) * WIDTH;
             indices[ix++] = (i + 1) + (j + 0) * WIDTH;
             indices[ix++] = (i + 1) + (j + 1) * WIDTH;
+
         }
     }
 
-    //for (size_t i = 0; i < WIDTH * HEIGHT * 4; i += 4) {
+    //for (size_t i = 0; i < ix; i += 4) {
         //int a = indices[i + 0];
         //int b = indices[i + 1];
         //int c = indices[i + 2];
@@ -194,60 +124,86 @@ int main(int argc, char const *argv[])
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+    GLint status;
+    GLint infoLogLength;
+    GLchar *strInfoLog;
+
     // COMPILE VERTEX SHADER
-    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vShader, 1, &vertexShader, NULL);
-    glCompileShader(vShader);
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, 0);
+    glCompileShader(vertexShader);
+
+    // CHECK COMPILE STATUS
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &infoLogLength);
+        strInfoLog = new GLchar[infoLogLength + 1];
+        glGetShaderInfoLog(vertexShader, infoLogLength, NULL, strInfoLog);
+        printf("%s\n", strInfoLog);
+        delete[] strInfoLog;
+    }
 
     // COMPILE FRAGMENT SHADER
-    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fShader, 1, &fragmentShader, NULL);
-    glCompileShader(fShader);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, 0);
+    glCompileShader(fragmentShader);
+
+    // CHECK COMPILE STATUS
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &infoLogLength);
+        strInfoLog = new GLchar[infoLogLength + 1];
+        glGetShaderInfoLog(fragmentShader, infoLogLength, NULL, strInfoLog);
+        printf("%s\n", strInfoLog);
+        delete[] strInfoLog;
+    }
 
     // LINK SHADERS
     GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vShader);
-    glAttachShader(shaderProgram, fShader);
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
     glUseProgram(shaderProgram);
 
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    // CHECK LINK STATUS
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE) {
+        glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
+        strInfoLog = new GLchar[infoLogLength + 1];
+        glGetProgramInfoLog(shaderProgram, infoLogLength, NULL, strInfoLog);
+        printf("Linker failure: %s\n", strInfoLog);
+        delete[] strInfoLog;
+    }
 
     // PROJECTION MATRIX
     glm::mat4 proj = glm::perspective(45.0f, 1.5f, 1.0f, 1000.0f);
     GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
     glUniformMatrix4fv(uniProj, 1.0, GL_FALSE, glm::value_ptr(proj));
 
-    // VIEW MATRIX
-    glm::mat4 view = glm::lookAt(
-        glm::vec3(0.0f, 10.0f, 0.0f),
-        glm::vec3(5.0f, 0.0f, 5.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f)
-    );
-    GLint uniView = glGetUniformLocation(shaderProgram, "view");
-    glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
-
     do {
         // MODEL MATRIX
         glm::mat4 model;
-        GLint uniModel = glGetUniformLocation(shaderProgram, "model");
         model = glm::rotate(model, g_camera.rotX, glm::vec3(1.0f, 0.0f, 0.0f));
         model = glm::rotate(model, g_camera.rotY, glm::vec3(0.0f, 1.0f, 0.0f));
-        model = glm::translate(model, glm::vec3(g_camera.x, g_camera.y, g_camera.z));
+        model = glm::translate(model, g_camera.position);
+        GLint uniModel = glGetUniformLocation(shaderProgram, "model");
         glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
 
         // CLEAR BUFFERS AND RENDER
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glDrawElements(GL_LINE_STRIP, WIDTH * HEIGHT * 4, GL_UNSIGNED_INT, NULL);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glDrawElements(GL_TRIANGLE_STRIP, ix, GL_UNSIGNED_INT, NULL);
+
         glfwSwapBuffers();
     } while(glfwGetKey(GLFW_KEY_ESC) != GLFW_PRESS);
 
     // FREE RESOURCES
     glDeleteProgram(shaderProgram);
-    glDeleteShader(fShader);
-    glDeleteShader(vShader);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
 
     glDeleteBuffers(1, &vbo);
     glDeleteVertexArrays(1, &vao);
