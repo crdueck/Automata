e9bdd4528d7d5974ee64bf0d86f04f31b221e371
Chris Dueck <cdueck93@hotmail.com>
2013-04-25 08:04:13 -0400

    camera controls now working

--- c/callbacks.hpp
+++ c/callbacks.hpp
@@ -1,18 +1,18 @@
-#include "GL/glfw.h"
+#include <GL/glfw.h>
+#include <glm/glm.hpp>
 
 void GLFWCALL keyCallback(int, int);
 void GLFWCALL mousePosCallback(int, int);
 void GLFWCALL windowSizeCallback(int, int);
 
 enum {
-    MOVE_FORWARD    = 0x01;
-    MOVE_BACKWARD   = 0x02;
-    MOVE_BACKWARD   = 0x04;
-    MOVE_LEFT       = 0x08;
-    MOVE_RIGHT      = 0x10;
-    MOVE_UP         = 0x20;
-    MOVE_DOWN       = 0x40;
-}
+    MOVE_FORWARD    = 0x01,
+    MOVE_BACKWARD   = 0x02,
+    MOVE_LEFT       = 0x04,
+    MOVE_RIGHT      = 0x08,
+    MOVE_UP         = 0x10,
+    MOVE_DOWN       = 0x20,
+};
 
 typedef struct camera_t
 {
@@ -20,7 +20,7 @@ typedef struct camera_t
     float rotX;
     float rotY;
     float speed;
-    char movement;
+    uint8_t movement;
 } Camera;
 
 #define INIT_CAMERA(camera) do \
@@ -29,5 +29,5 @@ typedef struct camera_t
     (camera).rotX = 0.0f; \
     (camera).rotY = 0.0f; \
     (camera).movement = 0; \
-    (camera).speed = 3.0f; \
+    (camera).speed = 50.0f; \
 } while(0)
