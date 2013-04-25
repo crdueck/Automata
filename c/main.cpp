0c1f08fd6d0f0c8c8c1148082c48de4eb97aebca
Chris Dueck <cdueck93@hotmail.com>
2013-04-25 08:05:36 -0400

    fog, new updateWorld() with camera controls

--- c/main.cpp
+++ c/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <cassert>
 #include <GL/glew.h>
 #include <GL/glfw.h>
@@ -9,25 +10,28 @@
 #include "callbacks.hpp"
 
 const char *vertexSource =
-    "#version 120\n"
-    //"in vec3 position;\n" // GLSL 3.30
-    "attribute vec3 position;\n" // GLSL 1.20
+    "#version 330\n"
+    "in vec3 position;\n" // GLSL 3.30
+    //"attribute vec3 position;\n" // GLSL 1.20
     "uniform mat4 model;\n"
     "uniform mat4 proj;\n"
-    //"out vec3 color;\n" // GLSL 3.30
-    "varying vec3 color;\n" // GLSL 1.20
+    "out vec3 color;\n" // GLSL 3.30
+    //"varying vec3 color;\n" // GLSL 1.20
     "void main() {\n"
     "   gl_Position = proj * model * vec4(position, 1.0);\n"
-    "   color = position;\n"
+    "   color = position / 256.0f;\n"
     "}";
 
 const char *fragmentSource =
-    "#version 120\n"
-    //"in vec3 color;\n" // GLSL 3.30
-    "varying vec3 color;\n" // GLSL 1.20
+    "#version 330\n"
+    "in vec3 color;\n" // GLSL 3.30
+    //"varying vec3 color;\n" // GLSL 1.20
     "void main() {\n"
-    "   gl_FragColor = vec4(0.0, 0.0, color.b, 1.0);\n"
+    //"   gl_FragColor = vec4(0.0, 0.0, color.b, 1.0);\n"
+    "   gl_FragColor = vec4(color, 1.0);\n"
     "}";
+
+Camera g_camera;
 
 void glInit()
 {
@@ -40,17 +44,56 @@ void glInit()
 
     glEnable(GL_FOG);
     glFogi(GL_FOG_MODE, GL_LINEAR);
-    glFogi(GL_FOG_START, 250);
-    glFogi(GL_FOG_END, 2000);
+    glFogf(GL_FOG_DENSITY, 0.2f);
+    glFogi(GL_FOG_START, 30);
+    glFogi(GL_FOG_END, 100);
+    glHint(GL_FOG_HINT, GL_NICEST);
 
-    float fogColor[4] = { 0.5f, 0.7f, 1.0f, 1.0f };
+    GLfloat fogColor[4] = { 0.5f, 0.7f, 1.0f, 1.0f };
     glFogfv(GL_FOG_COLOR, fogColor);
 }
 
-static const size_t WIDTH = 64;
-static const size_t HEIGHT = 64;
+void updateWorld(float dt)
+{
+    float moveSpeed = g_camera.speed * dt;
+    float phi = glm::radians(g_camera.rotY);
 
-Camera g_camera;
+    // UPDATE CAMERA
+    if (g_camera.movement & MOVE_FORWARD) {
+        g_camera.position.x -= moveSpeed * sin(phi);
+        g_camera.position.z += moveSpeed * cos(phi);
+    } else if (g_camera.movement & MOVE_BACKWARD) {
+        g_camera.position.x += moveSpeed * sin(phi);
+        g_camera.position.z -= moveSpeed * cos(phi);
+    }
+
+    if (g_camera.movement & MOVE_LEFT) {
+        g_camera.position.x += moveSpeed * cos(phi);
+        g_camera.position.z += moveSpeed * sin(phi);
+    } else if (g_camera.movement & MOVE_RIGHT) {
+        g_camera.position.x -= moveSpeed * cos(phi);
+        g_camera.position.z -= moveSpeed * sin(phi);
+    }
+
+    if (g_camera.movement & MOVE_UP) {
+        g_camera.position.y -= moveSpeed;
+    } else if (g_camera.movement & MOVE_DOWN) {
+        g_camera.position.y += moveSpeed;
+    }
+}
+
+float harmonic2D(size_t octave, float freq, float x, float y)
+{
+    float noise = 0.0f;
+    for (size_t i = 1; i < octave; i++) {
+        float r = 1 << (i - 1);
+        noise += glm::simplex(glm::vec2(x * r / freq, y * r / freq)) / r;
+    }
+    return noise / (2.0f - 1.0f / (2 ^ (octave - 1)));
+}
+
+static const size_t WIDTH = 256;
+static const size_t HEIGHT = 256;
 
 int main(int argc, char const *argv[])
 {
@@ -78,34 +121,29 @@ int main(int argc, char const *argv[])
             //int ix3 = i * 3 + j * 3 * WIDTH + 2;
             //printf("(%d, %d, %d)\n", ix1, ix2, ix3);
 
+            float noise = harmonic2D(5, 25.0f, i, j);
+            noise += harmonic2D(3, 45.0f, i, j);
+            noise += harmonic2D(3, 110.f, i, j);
+            noise += harmonic2D(5, 220.f, i, j);
+
             heightmap[i * 3 + j * 3 * WIDTH + 0] = (float)i;
-            heightmap[i * 3 + j * 3 * WIDTH + 1] = glm::simplex(glm::vec2(i, j));
+            heightmap[i * 3 + j * 3 * WIDTH + 1] = 10.0f * noise;
             heightmap[i * 3 + j * 3 * WIDTH + 2] = (float)j;
 
         }
     }
 
-    GLuint indices[WIDTH * HEIGHT * 4];
+    GLuint indices[(WIDTH - 1) * (HEIGHT - 1) * 4];
 
     size_t ix = 0;
-    for (size_t i = 0; i < WIDTH - 1; i++) {
-        for (size_t j = 0; j < HEIGHT - 1; j++) {
-
+    for (size_t i = 0; i < WIDTH - 2; i++) {
+        for (size_t j = 0; j < HEIGHT - 2; j++) {
             indices[ix++] = (i + 0) + (j + 0) * WIDTH;
             indices[ix++] = (i + 0) + (j + 1) * WIDTH;
             indices[ix++] = (i + 1) + (j + 0) * WIDTH;
             indices[ix++] = (i + 1) + (j + 1) * WIDTH;
-
         }
     }
-
-    //for (size_t i = 0; i < ix; i += 4) {
-        //int a = indices[i + 0];
-        //int b = indices[i + 1];
-        //int c = indices[i + 2];
-        //int d = indices[i + 3];
-        //printf("(%d, %d, %d, %d)", a, b, c, d);
-    //}
 
     // VERTEX ARRAY OBJECT
     GLuint vao;
@@ -124,39 +162,15 @@ int main(int argc, char const *argv[])
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    GLint status;
-    GLint infoLogLength;
-    GLchar *strInfoLog;
-
     // COMPILE VERTEX SHADER
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexSource, 0);
     glCompileShader(vertexShader);
 
-    // CHECK COMPILE STATUS
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
-    if (status == GL_FALSE) {
-        glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &infoLogLength);
-        strInfoLog = new GLchar[infoLogLength + 1];
-        glGetShaderInfoLog(vertexShader, infoLogLength, NULL, strInfoLog);
-        printf("%s\n", strInfoLog);
-        delete[] strInfoLog;
-    }
-
     // COMPILE FRAGMENT SHADER
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentSource, 0);
     glCompileShader(fragmentShader);
-
-    // CHECK COMPILE STATUS
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
-    if (status == GL_FALSE) {
-        glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &infoLogLength);
-        strInfoLog = new GLchar[infoLogLength + 1];
-        glGetShaderInfoLog(fragmentShader, infoLogLength, NULL, strInfoLog);
-        printf("%s\n", strInfoLog);
-        delete[] strInfoLog;
-    }
 
     // LINK SHADERS
     GLuint shaderProgram = glCreateProgram();
@@ -165,22 +179,20 @@ int main(int argc, char const *argv[])
     glLinkProgram(shaderProgram);
     glUseProgram(shaderProgram);
 
-    // CHECK LINK STATUS
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
-    if (status == GL_FALSE) {
-        glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
-        strInfoLog = new GLchar[infoLogLength + 1];
-        glGetProgramInfoLog(shaderProgram, infoLogLength, NULL, strInfoLog);
-        printf("Linker failure: %s\n", strInfoLog);
-        delete[] strInfoLog;
-    }
-
     // PROJECTION MATRIX
     glm::mat4 proj = glm::perspective(45.0f, 1.5f, 1.0f, 1000.0f);
     GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
     glUniformMatrix4fv(uniProj, 1.0, GL_FALSE, glm::value_ptr(proj));
 
+    double lastTime = glfwGetTime();
+
     do {
+        double currentTime = glfwGetTime();
+        double dt = currentTime - lastTime;
+        lastTime = currentTime;
+
+        updateWorld(dt);
+
         // MODEL MATRIX
         glm::mat4 model;
         model = glm::rotate(model, g_camera.rotX, glm::vec3(1.0f, 0.0f, 0.0f));
@@ -195,8 +207,8 @@ int main(int argc, char const *argv[])
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-        glDrawElements(GL_TRIANGLE_STRIP, ix, GL_UNSIGNED_INT, NULL);
 
+        glDrawElements(GL_TRIANGLE_STRIP, ix, GL_UNSIGNED_INT, NULL);
         glfwSwapBuffers();
     } while(glfwGetKey(GLFW_KEY_ESC) != GLFW_PRESS);
 
